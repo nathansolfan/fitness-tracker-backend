3435286d47743c503e123b6088238a095e90ec8a
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-19 20:09:32 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WorkoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/register', [AuthController::class, 'register']);
+Route::ge('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('workouts', WorkoutController::class);
 });
